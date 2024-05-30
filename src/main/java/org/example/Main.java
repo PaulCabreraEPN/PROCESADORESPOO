59251b7e855b23ea18eaba74004cb930dad4431c
TADIO <adrian.cadena@epn.edu.ec>
2024-05-30 08:38:16 -0500

    importacion de clase procesadores moviles

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import Procesadores.Procesadores;
-
+import Procesadores.Procesadores_moviles;
 public class Main {
     public static void main(String[] args) {
 
         Procesadores pi = new Procesadores();
+
+        //TADIO
+        Procesadores p = new Procesadores_moviles();
 
     }
 }
