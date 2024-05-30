f60b515a5c985692634ec7fdda7e21836ca6c1da
1-21-01-E043-11\POO <elkin.diaz@epn.edu.ec>
2024-05-30 08:43:00 -0500

    3.5.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import Procesadores.Procesadores_moviles;
 public class Main {
     public static void main(String[] args) {
 
+        //PaulCabrera
         Procesadores pi = new Procesadores();
 
         //TADIO
